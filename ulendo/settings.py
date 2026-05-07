10ee9b8dbcdc9038ee0f5fcbd819729085c72c94
chiumiavijay <chiumiavijay75@gmail.com>
2026-05-08 00:11:02 +0200

    Update settings.py

--- ulendo/settings.py
+++ ulendo/settings.py
@@ -137,19 +137,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# -------------------
-# EMAIL CONFIG (SECURE)
-# -------------------
+# 
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chiumiavijay75@gmail.com'
-EMAIL_HOST_PASSWORD = 'fyxuwqugjngnwsib'
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 
 # ========================
 # CUSTOM PROJECT SETTINGS

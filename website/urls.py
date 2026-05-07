448e939796b98c323aea619602e1c99441b6879d
chiumiavijay <chiumiavijay75@gmail.com>
2026-05-08 00:51:06 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -11,6 +11,9 @@ def redirect_to_dashboard(request):
 urlpatterns = [
 
 
+path('test-sms/', views.test_sms, name='test_sms'),
+
+
 
 
 path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
